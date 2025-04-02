dc0ca36e662e691fd50a8ccff8cfb42b5eff1e36
Dan Ngo <danngo.au@gmail.com>
2025-04-02 16:29:00 +1100

    add .editorconfig and change browser tab title

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,4 +1,10 @@
+import { useEffect } from "react";
+
 export default function About() {
+  useEffect(() => {
+    document.title = "About Page";
+  }, []);
+
   return (
     <>
       <div>About</div>
