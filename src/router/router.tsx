c76d15c12e1612eb1e53ffbc673db436bbbdbc56
XMcPhee <103607491@student.swin.edu.au>
2025-04-02 15:29:19 +1100

    Simple react router

---
+++ src/router/router.tsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter } from "react-router";
+
+import App from '../App.tsx';
+import Home from '../components/Home/Home.tsx';
+import NotFound from '../components/NotFound/NotFound.tsx';
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {
+                index: true,
+                path: "/",
+                element: <Home />
+            },
+            {
+                path: "/*",
+                element: <NotFound />
+            }
+        ]
+    },
+]);
+  
