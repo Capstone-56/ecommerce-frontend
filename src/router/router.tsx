101d8f6af6aaf0a6b76ab074d8f2ac4173931e75
XMcPhee <103607491@student.swin.edu.au>
2025-04-10 14:36:44 +1000

    Format

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -3,7 +3,7 @@ import { createBrowserRouter } from "react-router";
 import App from '../App.tsx';
 import Home from '../components/Home/Home.tsx';
 import About from "../components/About/About.tsx";
-import Profile from "../components/Profile/Profile.tsx"
+import Profile from "../components/Profile/Profile.tsx";
 import NotFound from '../components/NotFound/NotFound.tsx';
 
 export const router = createBrowserRouter([
