6235999fc82662a5a80520e86b25d1d0ffcfa07b
Dan Ngo <danngo.au@gmail.com>
2025-04-10 19:06:30 +1000

    PA-120 add routes and fetch product Id from URL

--- src/components/Product/ProductDetails.tsx
+++ src/components/Product/ProductDetails.tsx
@@ -1,16 +1,14 @@
+import { useParams } from "react-router-dom";
+
 import { ProductModel } from "@/models/ProductModel";
 
-interface ProductDetailsProps {
-  productId: string;
-}
+export default function ProductDetails() {
+  const productId = useParams().id;
+  console.log(productId);
 
-// My (Dan's) propose of how to approach this page: When clicking on a product card on the
-// Products page, the product ID will be bound either directly to this component's props,
-// or the URL (/products/:id/details) and this component would fetch the ID itself.
-// then it will call the API to get the product details from the server to display on the UI
-export default function ProductDetails({ productId } : ProductDetailsProps) {
   return (
     <>
+    hello
     </>
   );
 };
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -3,8 +3,10 @@ import { createBrowserRouter } from "react-router";
 import App from "@/App";
 import Home from "@/components/Home/Home";
 import About from "@/components/About/About";
+import Profile from "@/components/Profile/Profile";
+import Products from "@/components/Product/Products";
+import ProductDetails from "@/components/Product/ProductDetails";
 import NotFound from "@/components/NotFound/NotFound";
-import Profile from "@/components/Profile/Profile.tsx";
 
 export const router = createBrowserRouter([
     {
@@ -23,6 +25,14 @@ export const router = createBrowserRouter([
             {
                 path: "/profile",
                 element: <Profile />
+            },
+            {
+              path: "/products",
+              element: <Products />
+            },
+            {
+              path: "products/:id/details",
+              element: <ProductDetails />
             },
             {
                 path: "/*",
