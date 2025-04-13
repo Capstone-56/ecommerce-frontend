3c21362915864ce4788b6132bfe4f20e3f28f1ac
Dan Ngo <danngo.au@gmail.com>
2025-04-13 17:22:44 +1000

    PA-123 spelling fix

--- src/domain/constants.ts
+++ src/domain/constants.ts
@@ -1,4 +1,4 @@
-/** Constans for global usages */
+/** Constants for global usages */
 export class Constants {
   // Caching
   public static readonly LOCAL_STORAGE_EXAMPLE : string = "cache-example";
