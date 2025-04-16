651d2747ad22626a48c1f1062d241731c03f2f77
Dan Ngo <danngo.au@gmail.com>
2025-04-16 22:02:12 +1000

    resolve merge conflicts

--- src/components/Product/Products.tsx
+++ src/components/Product/Products.tsx
@@ -44,82 +44,89 @@ export default function Products() {
       <Box component="main" sx={{ flexGrow: 1 }}>
         <Container maxWidth="xl" sx={{ mt: 4, mb: 8 }}>
           {/* REPLACE: breadcrumb currently hardcoded. needs to be dynamic */}
-            <Breadcrumbs>
-              <Link to="/">Home</Link>
-              <Link to="/products">Products</Link>
-            </Breadcrumbs>
+          <Breadcrumbs>
+            <Link to="/">Home</Link>
+            <Link to="/products">Products</Link>
+          </Breadcrumbs>
 
-            {/* Responsive Flex Container */}
+          {/* Responsive Flex Container */}
+          <Box 
+            sx={{ 
+              display: "flex", 
+              flexDirection: { xs: "column", md: "row" }, 
+              gap: 3, 
+              mt: 2.5 
+            }}
+          >
+            {/* Filters section */}
             <Box 
               sx={{ 
-                display: "flex", 
-                flexDirection: { xs: "column", md: "row" }, 
-                gap: 3, 
-                mt: 2.5 
+                width: { xs: "100%", md: "240px" }, 
+                flexShrink: 0 
               }}
             >
-              {/* Filters section */}
-              <Box 
-                sx={{ 
-                  width: { xs: "100%", md: "240px" }, 
-                  flexShrink: 0 
-                }}
-              >
-                {/* Categories filter would be implemented here */}
-                <Paper elevation={0} sx={{ p: 2, border: "1px solid #e0e0e0", borderRadius: 3 }}>
-                  <Typography variant="h6" sx={{ mb: 2 }}>Filters</Typography>
-                  <Box sx={{ mb: 3 }}>
-                    <Typography variant="subtitle1" sx={{ mb: 1 }}>Categories</Typography>
-                    <Typography variant="subtitle1" sx={{ mb: 1 }}>Category 1</Typography>
-                    <Typography variant="subtitle1" sx={{ mb: 1 }}>Category 2</Typography>
-                    <Typography variant="subtitle1" sx={{ mb: 1 }}>Category 3</Typography>
-                    <Typography variant="subtitle1" sx={{ mb: 1 }}>Category 4</Typography>
-                    <Typography variant="subtitle1" sx={{ mb: 1 }}>Category 5</Typography>
-                    <Typography variant="subtitle1" sx={{ mb: 1 }}>Category 6</Typography>
-                  </Box>
-
-                  <Box sx={{ mb: 3 }}>
-                    {/* Price range filter would be implemented here */}
-                  </Box>
-                </Paper>
-              </Box>
-
-              {/* Product listing */}
-              <Box sx={{ flex: 1 }}>
-                <Box 
-                  sx={{ 
-                    display: "flex", 
-                    flexDirection: { xs: "column", sm: "row" }, 
-                    justifyContent: "space-between", 
-                    alignItems: { xs: "flex-start", sm: "center" }, 
-                    mb: 2.5 
-                  }}
-                >
-                  <Typography variant="body2" color="text.secondary">
-                    Showing {products.length} products
-                  </Typography>
-
-                  <Box sx={{ display: "flex", alignItems: "center", mt: { xs: 1, sm: 0 } }}>
-                    <Typography variant="body2" sx={{ mr: 1 }}>Sort by:</Typography>
-                    {/* Sort options to be implemented */}
-                  </Box>
+              {/* Categories filter would be implemented here */}
+              <Paper elevation={0} sx={{ p: 2, border: "1px solid #e0e0e0", borderRadius: 3 }}>
+                <Typography variant="h6" sx={{ mb: 2 }}>Filters</Typography>
+                <Box sx={{ mb: 3 }}>
+                  <Typography variant="subtitle1" sx={{ mb: 1 }}>Categories</Typography>
+                  <Typography variant="subtitle1" sx={{ mb: 1 }}>Category 1</Typography>
+                  <Typography variant="subtitle1" sx={{ mb: 1 }}>Category 2</Typography>
+                  <Typography variant="subtitle1" sx={{ mb: 1 }}>Category 3</Typography>
+                  <Typography variant="subtitle1" sx={{ mb: 1 }}>Category 4</Typography>
+                  <Typography variant="subtitle1" sx={{ mb: 1 }}>Category 5</Typography>
+                  <Typography variant="subtitle1" sx={{ mb: 1 }}>Category 6</Typography>
                 </Box>
 
-                <Grid container spacing={3}>
-                  {/* Product Grid to be inserted here*/}
-                </Grid>
-              </Box>
+                <Box sx={{ mb: 3 }}>
+                  {/* Price range filter would be implemented here */}
+                </Box>
+              </Paper>
             </Box>
 
-            {/* Pagination */}
-            <Box sx={{ display: "flex", justifyContent: "center", mt: 4 }}>
-              {/* Add pagination component */}
+            {/* Product listing */}
+            <Box sx={{ flex: 1 }}>
+              <Box 
+                sx={{ 
+                  display: "flex", 
+                  flexDirection: { xs: "column", sm: "row" }, 
+                  justifyContent: "space-between", 
+                  alignItems: { xs: "flex-start", sm: "center" }, 
+                  mb: 2.5 
+                }}
+              >
+                {products && (
+                  <Typography variant="body2" color="text.secondary">
+                    Showing {products.results.length} products
+                  </Typography>
+                )}
+
+                <Box sx={{ display: "flex", alignItems: "center", mt: { xs: 1, sm: 0 } }}>
+                  <Typography variant="body2" sx={{ mr: 1 }}>Sort by:</Typography>
+                  {/* Sort options to be implemented */}
+                </Box>
+              </Box>
+
+              <Grid container spacing={3}>
+                {/* Product Grid to be inserted here*/}
+              </Grid>
             </Box>
-          </Container>
-        </Box>
-        {/* Footer Section */}
-        <Footer />
+          </Box>
+
+          {/* Pagination component */}
+          <Box sx={{ display: "flex", justifyContent: "center", mt: 4 }}>
+            <Paginator
+              pagedData={products}
+              currentPage={currentPage}
+              onPageChange={handlePageChange}
+            />
+          </Box>
+        </Container>
       </Box>
+      
+      {/* Footer Section */}
+      <Footer />
+    </Box>
     </>
   );
 };
