9a8927210c0485d90a420fabfc5d5e5b43bd61ef
Dan Ngo <danngo.au@gmail.com>
2025-04-17 17:31:35 +1000

    PA-137 created user and admin layouts

---
+++ src/resources/layouts/UserLayout.tsx
@@ -0,0 +1,18 @@
+import { Outlet } from "react-router-dom";
+
+import Navbar from "../components/Navigation/Navigation";
+import Footer from "../components/Footer/Footer";
+
+export default function UserLayout() {
+  return (
+    <>
+      <Navbar />
+
+      <div className="layout">
+        <Outlet />
+      </div>
+      
+      <Footer />
+    </>
+  );
+};
--- src/resources/pages/Categories/Categories.tsx
+++ src/resources/pages/Categories/Categories.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { Typography } from "@mui/material";
-import Navbar from "../Navigation/Navigation";
 
 const Categories: React.FC = () => {
   useEffect(() => {
@@ -9,7 +8,6 @@ const Categories: React.FC = () => {
 
   return (
     <div>
-      <Navbar />
       <Typography variant="h1">Categories</Typography>
     </div>
   );
