34c88b8b962d1f771afe6a514b1a584c82c5c4c9
Tony Lam <ku1a.lam27@gmail.com>
2025-05-09 11:08:48 +1000

    feat(PA-131): update product details

--- src/resources/pages/Product/ProductDetails.tsx
+++ src/resources/pages/Product/ProductDetails.tsx
@@ -12,13 +12,19 @@ import {
   Typography,
   ImageList,
   ImageListItem,
+  Divider,
+  ButtonBase,
 } from "@mui/material";
+
+const PLACEHOLDER_COLORS = ["green", "red", "blue", "brown"];
+const PLACEHOLDER_SIZES = ["sm", "md", "lg"];
 
 export default function ProductDetails() {
   const [productDetails, setProductDetails] = useState<ProductModel>();
   const [collection, setCollection] = useState<string[]>();
+  const [colour, setColour] = useState<string>();
   const { id: productId = "null" } = useParams();
-  const { name, description, images } = productDetails || {};
+  const { name, description, images, price } = productDetails || {};
 
   useEffect(() => {
     fetchProductDetails(productId);
@@ -43,61 +49,113 @@ export default function ProductDetails() {
   // DEBUG
   console.log(productDetails);
   console.log(collection);
-  console.log(location);
 
   return (
-    <Container maxWidth="xl" sx={{ margin: "4rem" }}>
-      {/* Breadcrumbs - needs to be dynamic */}
-      <Breadcrumbs>
-        <Link to="/">Home</Link>
-        <Link to="/products">Products</Link>
-        {/* Category should be here if coming from that route */}
-        <Link to={`/products/${productId}/details`}>{name}</Link>
-      </Breadcrumbs>
-
-      <Box>
-        {/* img list */}
-        <Box>
-          <ImageList
-            variant="quilted"
-            cols={3}
-            rowHeight={150}
-            gap={4}
-            sx={{ width: "50%", height: "50%" }}
-          >
-            {images.length > 0 ? (
-              images.map((link, index) =>
-                index === 0 ? (
-                  <ImageListItem key={`image-${index}`} cols={3}>
-                    <img
-                      src={`${link}`}
-                      alt="image alt text"
-                      style={{ objectFit: "cover" }}
-                    />
-                  </ImageListItem>
-                ) : (
-                  <ImageListItem>
-                    <img src={`${link}`} alt="image alt text" />
-                  </ImageListItem>
-                )
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: { xs: "column", md: "row" },
+        justifyContent: { md: "center" },
+        margin: "2rem",
+        paddingX: { xs: "2rem", md: "6rem" },
+        gap: "2rem",
+      }}
+    >
+      {/* img list */}
+      <Box sx={{ flex: "1 1 auto" }}>
+        <ImageList
+          variant="quilted"
+          cols={3}
+          rowHeight={140}
+          gap={4}
+          sx={{
+            marginLeft: { md: "auto" },
+          }}
+        >
+          {Array.isArray(images) && images.length > 0 ? (
+            images.map((link, index) =>
+              index === 0 ? (
+                <ImageListItem key={`image-${index}`} cols={3} rows={3}>
+                  <img
+                    src={`${link}`}
+                    alt="image alt text"
+                    style={{ objectFit: "cover", borderRadius: "16px" }}
+                  />
+                </ImageListItem>
+              ) : (
+                <ImageListItem key={`image-${index}`}>
+                  <img
+                    src={`${link}`}
+                    alt="image alt text"
+                    style={{ objectFit: "cover", borderRadius: "8px" }}
+                  />
+                </ImageListItem>
               )
-            ) : (
-              <Box>
-                <Typography>No images available</Typography>
-              </Box>
-            )}
-          </ImageList>
+            )
+          ) : (
+            <Box>
+              <Typography>No images available</Typography>
+            </Box>
+          )}
+        </ImageList>
+      </Box>
 
-          {/* Dialog or modal for extra images? */}
-        </Box>
+      {/* details */}
+      <Box
+        sx={{
+          flex: "1 1 auto",
+          flexDirection: "column",
+        }}
+      >
+        <Typography
+          variant="h2"
+          sx={{
+            color: "black",
+            fontWeight: "400",
+            fontSize: { xs: "48px", md: "36px" },
+          }}
+        >
+          {name}
+        </Typography>
+        <Typography
+          variant="caption"
+          sx={{ color: "black", fontSize: "1.5rem", marginTop: "1rem" }}
+        >
+          $25.00
+        </Typography>
+        <Divider orientation="horizontal" flexItem />
 
-        {/* item desc & variants etc */}
-        <Box>
-          <Typography variant="h2" sx={{ color: "black" }}>
-            {name}
-          </Typography>
+        <Typography variant="caption" sx={{ color: "grey" }}>
+          {description}
+        </Typography>
+        <Box sx={{ marginY: "2rem" }}>
+          {PLACEHOLDER_COLORS.map((color) => (
+            <ButtonBase
+              key={color}
+              onClick={() => {
+                console.log(color);
+                setColour(color);
+              }}
+              sx={{
+                width: "60px",
+                height: "60px",
+                backgroundColor: color,
+                borderRadius: "8px",
+                margin: "2px",
+              }}
+            >
+              <Box
+                sx={{
+                  color: { color },
+                  width: "40px",
+                  height: "40px",
+                  objectFit: "cover",
+                }}
+              ></Box>
+            </ButtonBase>
+          ))}
         </Box>
       </Box>
-    </Container>
+    </Box>
   );
 }
