1bfb6fb5a555634b9955f3847490107d4352eb12
DeacanWhite <whitedeacan@gmail.com>
2025-06-04 18:27:23 +1000

    style search component

--- src/resources/components/Search/SearchBar.tsx
+++ src/resources/components/Search/SearchBar.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useSearchParams, useNavigate, useLocation } from "react-router-dom";
-import { TextField, IconButton, Box } from "@mui/material";
+import { TextField, InputAdornment, Box } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 
 const SearchBar: React.FC = () => {
@@ -30,18 +30,57 @@ const SearchBar: React.FC = () => {
   }, [location.pathname]);
 
   return (
-    <Box sx={{ display: "flex", alignItems: "center", padding: "2px" }}>
-      <IconButton onClick={handleSearch} color="primary">
-        <SearchIcon />
-      </IconButton>
+    <Box sx={{ display: "flex", alignItems: "center", width: "100%", maxWidth: 200}}>
       <TextField
         variant="outlined"
-        size="small"
-        placeholder="Search products..."
+        size="medium"
+        placeholder="Search"
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
         onKeyDown={(e) => {
           if (e.key === "Enter") handleSearch();
+        }}
+        fullWidth
+        // alternatives to inputProps are also deprecated or dont allow sx... I hate MUI
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <SearchIcon 
+                sx={{ 
+                  color: 'grey.600',
+                  cursor: 'pointer'
+                }}
+                onClick={handleSearch}
+              />
+            </InputAdornment>
+          ),
+          sx: {
+            borderRadius: '50px',
+            backgroundColor: '#FDFDFD',
+            height: '40px',
+            '& .MuiOutlinedInput-notchedOutline': {
+              borderColor: 'grey.300',
+              borderWidth: '1px',
+            },
+            '&:hover .MuiOutlinedInput-notchedOutline': {
+              borderColor: 'grey.400',
+            },
+            '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+              borderColor: 'primary.main',
+              borderWidth: '2px',
+            },
+            paddingLeft: '14px', 
+          }
+        }}
+        sx={{
+          '& .MuiInputBase-input': {
+            padding: '12px 0 12px 0',
+            fontSize: '16px',
+          },
+          '& .MuiInputBase-input::placeholder': {
+            color: 'grey.500',
+            opacity: 1,
+          }
         }}
       />
     </Box>
