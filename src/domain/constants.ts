19d7ffaa4c5a4e4eaef53aefb9f9874a80919ce2
Dan Ngo <danngo.au@gmail.com>
2025-07-26 19:20:10 +1000

    Update constants.ts

--- src/domain/constants.ts
+++ src/domain/constants.ts
@@ -3,7 +3,7 @@ export class Constants {
   // Caching
   public static readonly LOCAL_STORAGE_EXAMPLE: string = "cache-example";
 
-  /** Storage name to be used in browser for cart state, only for un-authenticated users.  */
+  /** Storage name to be used in browser for cart state.  */
   public static readonly LOCAL_STORAGE_CART_STORAGE: string = "cart-storage";
   /** Storage name to be used in browser for authentication state.  */
   public static readonly LOCAL_STORAGE_AUTHENTICATION_STORAGE: string = "authenticated";
