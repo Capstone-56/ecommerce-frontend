f936e4ff9791b417b38e486407d0cdbc553bf796
XMcPhee <103607491@student.swin.edu.au>
2025-09-22 14:25:40 +1000

    PA-XXXX: Updates to menu content to reflect remaining pages

---
+++ src/resources/components/AdminPage/CategoryManagement.tsx
@@ -0,0 +1,11 @@
+/**
+ * Category Management page that will help admins manage
+ * their categories.
+ */
+export default function CategoryManagement() {
+  return (
+    <>
+      <div>Category Management</div>
+    </>
+  );
+};
