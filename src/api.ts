a5f53345da0127cd1c08443c18993c1dd72a4b99
Dan Ngo <danngo.au@gmail.com>
2025-10-05 16:29:08 +1100

    env awareness

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,18 @@
 import axios from "axios";
 
+function getApiBaseUrl(): string {
+  const hostname = window.location.hostname;
+  
+  if (hostname.includes("localhost")) {
+    return "http://localhost:8000/";
+  }
+
+  // Staging environment
+  return "https://api.bdnx.com/";
+};
+
 const api = axios.create({
-  baseURL: "https://api.bdnx.com/",
+  baseURL: getApiBaseUrl(),
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
